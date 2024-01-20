608e56b89fb525f7efbdbd0f36bfa92d837fa841
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-01-20 13:30:13 +0600

    alphabet hove effect update

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import Form from "./components/Form/Form";
 import BoxAnimation from "./components/animation/BoxAnimation/BoxAnimation";
 import Button from "./components/ui/Button";
 import Container from "./components/ui/Container";
+import "./App.css";
 
 const App = () => {
+  const name = "hover Me"
+  const nameArr = name.split("")
+  
   return (
       <Container>
       {/* <Button border={true} className={"bg-green-500"}/> */}
       <Button  className={""}>new</Button>
       <BoxAnimation/>
+      <div className="alphabetContainer">
+        {
+          nameArr.map((letter, index) => {
+            return <span key={index} className="alphabet" style={{transitionDelay: `${index * 50}ms`}}>{letter}</span>
+          })
+        }
+      </div>
       <Form/>
       </Container>
   
